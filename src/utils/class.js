112640b6fbe6d5c83175b7e5c9fdef0497bd09bf
damien-pro11-win10 <jyh91517@gmail.com>
2020-08-20 22:14:07 +0100

    add comments in Class

--- src/utils/class.js
+++ src/utils/class.js
@@ -1,30 +1,38 @@
 export class Node {
-    /**
-     * Class structure for each neuron node.
-     * 
-     * @param {string} layerName Name of the node's layer.
-     * @param {int} index Index of this node in its layer.
-     * @param {string} type Node type {input, conv, pool, relu, fc}. 
-     * @param {number} bias The bias assocated to this node.
-     * @param {[[number]]} output Output of this node.
-     */
-    constructor(layerName, index, type, bias, output) {
-      this.layerName = layerName;
-      this.index = index;
-      this.type = type;
-      this.bias = bias;
-      this.output = output;
-  
-      // Weights are stored in the links
-      this.inputLinks = [];
-      this.outputLinks = [];
-    }
+  /**
+   * Class structure for each neuron node.
+   * 
+   * @param {string} layerName Name of the node's layer.
+   * @param {int} index Index of this node in its layer.
+   * @param {string} type Node type {input, embedding, lstm, fc, etc...}. 
+   * @param {number} bias The bias assocated to this node.
+   * @param {number[] or number} output Output of this node.
+   */
+  constructor(layerName, index, type, bias, output) {
+    this.layerName = layerName;
+    this.index = index;
+    this.type = type;
+    this.bias = bias;
+    this.output = output;
+
+    // Weights are stored in the links
+    this.inputLinks = [];
+    this.outputLinks = [];
   }
-  
-  export class Link {
-    constructor(source, dest, weight) {
-      this.source = source;
-      this.dest = dest;
-      this.weight = weight;
-    }
+}
+
+export class Link {
+  /**
+   * Class structure for each link between two nodes.
+   * 
+   * @param {Node} source Source node.
+   * @param {Node} dest Target node.
+   * @param {number} weight Weight associated to this link. It can be a number,
+   *  1D array, or 2D array.
+   */
+  constructor(source, dest, weight) {
+    this.source = source;
+    this.dest = dest;
+    this.weight = weight;
   }
+}
